3baf2d790a6c667775e49c75de21601e59b4ae2d
Rumana <rumanaparven16@gmail.com>
2020-11-02 22:11:59 +0530

    [Rumana] Add. Update salary

--- EmployeePayrollService/EmployeeRepo.cs
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -17,46 +17,20 @@ namespace EmployeePayrollService
                 EmployeePayroll employeePayroll = new EmployeePayroll();
                 using (this.connection)
                 {
-                    string query = @"select e.empID,e.name,d.deptName,e.salary,e.start_Date,e.address,p.basic_pay,p.income_tax from employee e join department d
-                                    on e.deptID=d.deptID join payroll p on e.empID=p.empID where e.name='Priya'";
+                    string query = @"update payroll set basic_pay=3000000.00 where empID=(select empID from employee where name='Priya') ";
                     SqlCommand cmd = new SqlCommand(query, this.connection);
                     this.connection.Open();
 
-                    SqlDataReader dr = cmd.ExecuteReader();
-                    if (dr.HasRows)
+                    int rows = cmd.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (rows > 0)
                     {
-                        while (dr.Read())
-                        {
-                            employeePayroll.EmployeeID = dr.GetInt32(0);
-                            employeePayroll.EmployeeName = dr.GetString(1);
-                            employeePayroll.department = dr.GetString(2);
-                            employeePayroll.salary = Convert.ToDouble(dr.GetDecimal(3));
-                            employeePayroll.startDate = dr.GetDateTime(4);
-                            employeePayroll.address = dr.GetString(5);
-                            employeePayroll.BasicPay= Convert.ToDouble(dr.GetDecimal(6));
-                            employeePayroll.tax= Convert.ToDouble(dr.GetDecimal(7));
-
-                          
-                            Console.WriteLine(employeePayroll.EmployeeID+" "+ employeePayroll.EmployeeName+" "+ employeePayroll.department+" "+ employeePayroll.salary+
-                                " "+ employeePayroll.startDate+" "+ employeePayroll.address+" "+ employeePayroll.BasicPay+" "+ employeePayroll.tax);
-                            //Console.WriteLine(employeePayroll.EmployeeName);
-                            //Console.WriteLine(employeePayroll.department);
-                            //Console.WriteLine(employeePayroll.salary);
-                            //Console.WriteLine(employeePayroll.startDate);
-                            //Console.WriteLine(employeePayroll.address);
-                            //Console.WriteLine(employeePayroll.BasicPay);
-                            //Console.WriteLine(employeePayroll.tax);
-
-
-
-                        }
+                        Console.WriteLine(rows + " row(s) affected");
                     }
                     else
                     {
-                        Console.WriteLine("No data found");
+                        Console.WriteLine("Please check your query");
                     }
-                    dr.Close();
-                    this.connection.Close();
                 }
             }
             catch(Exception e)
